a36b324633fcd9dc6efc68d327099e9059716381
tzlilhaim <tzlilhaim480@gmail.com>
2020-09-09 12:12:41 +0300

    Added dist folder

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const port = 8080
+const path = require("path")
+
+app.use(express.static(path.join(__dirname,"dist")))
 
 app.listen(port, function () {
   console.log(`running server on port ${port}`)
